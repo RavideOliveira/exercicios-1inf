5025dcc2dcd5cbdfc5c326669b112ad02f295d89
Ravi <oravi2945@gmail.com>
2026-09-12 02:21:32 +0000

    refazendo-projeto-01

--- 3-bimestre/logica-de-programacao/projetos/projeto1/index.js
+++ 3-bimestre/logica-de-programacao/projetos/projeto1/index.js
@@ -1,10 +1,39 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
+// INFORMAÇÕES DA CLIENTE
+const cliente = "Mariana Silva"
+const cidade = "Fortaleza"
+const produto = "Notebook Pro"
+const categoria = "Notebook"
+const preco = 3500
+const quantidade = 2
+const descontoPercentual = 10
+const valorPago = 7000
+const subtotal = preco * quantidade
+const valorDesconto = subtotal * descontoPercentual / 100
+const valorFinal = subtotal - valorDesconto
+const troco = valorPago - valorFinal
+const resumo = `TECHSTORE SISTEMAS
+RESUMO DO PEDIDO
+Cliente: ${cliente}
+Cidade: ${cidade}
 
+Produto: ${produto}
+Categoria: ${categoria}
+Quantidade: ${quantidade}
+Preço unitário: R$ ${preco}
 
+Subtotal: R$ ${subtotal}
+Desconto: R$ ${descontoPercentual}
+Valor do desconto: ${valorDesconto}
+Valor final: R$ ${valorFinal}
 
+Valor pago: R$ ${valorPago}
+Troco: R$ ${troco}
 
-
-
+Obrigado pela compra!
+`
+console.log(resumo)
+// ===NÃO EXECUTAR O CÓDIGO NO TERMINAL=====
 // === FIM DO CÓDIGO =======================
 // === NÃO FAZER NADA ABAIXO DESSA LINHA ===
 module.exports = {
